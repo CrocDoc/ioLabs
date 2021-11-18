73897510b65eb51223840e9f639a5fb8cd67de82
Daniel Chorouš <danielchorous@gmail.com>
2021-11-18 14:04:10 +0100

    Repo init

---
+++ Utils/ExchangeRateDeserializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ExchangeRateUpdater
+{
+    public static class ExchangeRateDeserializer
+    {
+        private const char _lineSeparator = '\n';
+        private const char _lineValuesSeparator = '|';
+        private const int _currencyCodeArrayPosition = 3;
+        private const int _rateArrayPosition = 4;
+
+        public static IEnumerable<ExchangeRate> DeserializeCNBExchangeRateString(string input)
+        {
+            var output = new List<ExchangeRate>();
+
+            var inputLines = input.Split(_lineSeparator, StringSplitOptions.RemoveEmptyEntries).Skip(2);
+
+            foreach (var line in inputLines)
+            {
+                var lineValues = line.Split(_lineValuesSeparator);
+
+                var exchangeRate = new ExchangeRate(
+                    new Currency("CZK"), 
+                    new Currency(lineValues[_currencyCodeArrayPosition]),
+                    decimal.Parse(lineValues[_rateArrayPosition], CultureInfo.GetCultureInfo("cs-CZ")));
+
+                output.Add(exchangeRate);
+            }
+
+            return output;
+        }
+    }
+}
